7d9fb2a394d9fb482a0d1fa0204a388fd64ceb04
programerkkkkkkkkkkkkkkkkkkk <hipitelo@outlook.com>
2023-03-03 03:41:00 +0900

    Project's Initial Commit

---
+++ www/components/login.js
@@ -0,0 +1,13 @@
+// This is a JavaScript file
+
+function olamundo() { alert('olamundo');}
+
+function log(){
+var log;var h1;
+
+log=document.getElementById(log).value;
+h1=document.getElementById(log).value;
+alert(h1);
+
+}
+
